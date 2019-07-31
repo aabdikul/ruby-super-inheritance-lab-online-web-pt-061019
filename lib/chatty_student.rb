090d8c11780100e6f70f630f2d645a1fc10d5a42
Aida Abdikulova <abdikulova@gmail.com>
2019-07-31 05:02:42 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -4,12 +4,14 @@ class ChattyStudent < Student
     puts "Hey there! I'm so excited to learn stuff."
     super
     puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+    end
   end
 
   def raise_hand
     puts "Pick me!"
     super
     9.times { puts "Pick me!"}
+    end
   end
   
   end
